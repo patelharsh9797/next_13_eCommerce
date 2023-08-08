e6c99679772a5a8843ff1cde090bde65c30cdc24
Harsh Patel <105432105+patelharsh9797@users.noreply.github.com>
2023-08-08 10:53:41 +0530

    added style to product card

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -3,11 +3,15 @@ import formatPrice from "@/utils/PriceFormat";
 import {ProductType} from "@/types/ProductType";
 
 
-const Product = ({name,price,image}:ProductType) => {
+const Product = ({name, price, image}: ProductType) => {
     return (
-        <div>
-            <Image src={image} alt={name} width={400} height={400} />
-            <h1>{name} - {price ? formatPrice(price) : "N/A"}</h1>
+        <div className="text-gray-700">
+            <Image src={image} alt={name} width={800} height={800} className="w-full h-96 object-cover rounded-lg"/>
+
+            <div className="font-medium py-2">
+                <h1>{name}</h1>
+                <h2 className="text-sm text-teal-700">{price ? formatPrice(price) : "N/A"}</h2>
+            </div>
         </div>
     )
 }

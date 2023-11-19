9dd744f00213d877ee17ca82a782ba815b54f83b
Harsh Patel <patelharsh9797@users.noreply.github.com>
2023-11-19 21:24:42 +0530

    Added the progressbar

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -10,66 +10,75 @@ import Link from "next/link";
 import { AiFillShopping } from "react-icons/ai";
 import { AnimatePresence, motion } from "framer-motion";
 import Hydrate from "./Hydrate";
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 
 export default function Nav({ user }: Session) {
   const cartStore = userCartStore();
 
   return (
-    <nav className="flex justify-between items-center py-12">
-      <Link href={"/"}>
-        <h1 className="font-bold text-xl">Styled</h1>
-      </Link>
-      <ul className="flex items-center gap-8">
-        <li
-          onClick={cartStore.toggleCart}
-          className="flex items-center text-3xl relative cursor-pointer"
-        >
-          <AiFillShopping />
-          <AnimatePresence>
-            <Hydrate className="w-5 h-5 rounded-full absolute left-4 bottom-4 flex justify-center items-center">
-              {cartStore.cart.length > 0 && (
-                <motion.span
-                  initial={{ scale: 0 }}
-                  animate={{ scale: 1 }}
-                  exit={{ scale: 0 }}
-                  className="bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-4 bottom-4 flex justify-center items-center"
-                >
-                  {cartStore.cart.length}
-                </motion.span>
-              )}
-            </Hydrate>
-          </AnimatePresence>
-        </li>
-        {!user && (
-          <li>
-            <button
-              onClick={() => signIn()}
-              className="bg-teal-600 text-white py-2 px-4 rounded-md"
-            >
-              Sign In
-            </button>
+    <>
+      <ProgressBar
+        height="4px"
+        color="#0d9488"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+      <nav className="flex justify-between items-center py-12">
+        <Link href={"/"}>
+          <h1 className="font-bold text-xl">Styled</h1>
+        </Link>
+        <ul className="flex items-center gap-8">
+          <li
+            onClick={cartStore.toggleCart}
+            className="flex items-center text-3xl relative cursor-pointer"
+          >
+            <AiFillShopping />
+            <AnimatePresence>
+              <Hydrate className="w-5 h-5 rounded-full absolute left-4 bottom-4 flex justify-center items-center">
+                {cartStore.cart.length > 0 && (
+                  <motion.span
+                    initial={{ scale: 0 }}
+                    animate={{ scale: 1 }}
+                    exit={{ scale: 0 }}
+                    className="bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-4 bottom-4 flex justify-center items-center"
+                  >
+                    {cartStore.cart.length}
+                  </motion.span>
+                )}
+              </Hydrate>
+            </AnimatePresence>
           </li>
-        )}
-
-        {!!user && (
-          <>
+          {!user && (
             <li>
-              <Image
-                className="rounded-full"
-                src={user?.image as string}
-                alt={user?.name as string}
-                width={36}
-                height={36}
-              />
+              <button
+                onClick={() => signIn()}
+                className="bg-teal-600 text-white py-2 px-4 rounded-md"
+              >
+                Sign In
+              </button>
             </li>
-            <li className="bg-teal-600 text-white py-2 px-4 rounded-md">
-              <button onClick={() => signOut()}>Sign Out</button>
-            </li>
-          </>
-        )}
-      </ul>
+          )}
 
-      <AnimatePresence>{cartStore.isOpen && <Cart />}</AnimatePresence>
-    </nav>
+          {!!user && (
+            <>
+              <li>
+                <Image
+                  className="rounded-full"
+                  src={user?.image as string}
+                  alt={user?.name as string}
+                  width={36}
+                  height={36}
+                />
+              </li>
+              <li className="bg-teal-600 text-white py-2 px-4 rounded-md">
+                <button onClick={() => signOut()}>Sign Out</button>
+              </li>
+            </>
+          )}
+        </ul>
+
+        <AnimatePresence>{cartStore.isOpen && <Cart />}</AnimatePresence>
+      </nav>
+    </>
   );
 }
